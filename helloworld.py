b83e41b16b470b6f33d6349d4a389147020d70d3
Danie <150205709+daniemcclanahan-pvcc@users.noreply.github.com>
2023-11-27 22:11:54 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# Name: Danie-McClanahan
+# Prog Purpose: Hello World
+
+print("Hello World! This is my firsy Python program.")
+print("I can program in HTML, which is awesome.")
+print("However, HTML cannot do arithmetic.")
+print("But Python can: \n\t Add \n\t Multiply \n\t Divide")
+print("and lots of other things!")
